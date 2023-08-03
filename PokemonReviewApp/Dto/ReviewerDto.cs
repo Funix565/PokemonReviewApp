b1ba666763b0028c9b7a84b791c5873dc0ebc942
Funix565 <denkin.dmitriy@gmail.com>
2023-08-03 19:32:47 +0300

    Add project files.

---
+++ PokemonReviewApp/Dto/ReviewerDto.cs
@@ -0,0 +1,15 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Dto
+{
+    public class ReviewerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // TODO: Fix for JSON cycles in `GetReviewsByReviewer`. We want to get a Reviewer and see the array of his reviews.
+        //  But at the same time Review object also has reference to the Reviewer -> and here we have nesting.
+        // public ICollection<ReviewDto> Reviews { get; set; }
+    }
+}
